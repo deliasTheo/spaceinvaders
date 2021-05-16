ec710c288ad74a1589e18542ccb50e2a44b6360c
Theo Delias <t.delias@gmail.com>
2021-05-16 22:33:02 +0200

    'Etape3'

--- src/test/java/fr/unilim/iut/spaceinvaders/SpaceInvadersTest.java
+++ src/test/java/fr/unilim/iut/spaceinvaders/SpaceInvadersTest.java
@@ -48,27 +48,7 @@
 			"...............\n" + 
 			"...............\n" + 
 			".......V.......\n" , spaceinvaders.recupererEspaceJeuDansChaineASCII());
-		}
-	   
-	   @Test
-		public void test_VaisseauAvance_DeplacerVaisseauVersLaDroite() {
-			
-			spaceinvaders.positionnerUnNouveauVaisseau(1,1,7,9);
-
-			spaceinvaders.deplacerVaisseauVersLaDroite();
-			
-			assertEquals("" + 
-			"...............\n" + 
-			"...............\n" +
-			"...............\n" + 
-			"...............\n" + 
-			"...............\n" + 
-			"...............\n" + 
-			"...............\n" + 
-			"...............\n" + 
-			"...............\n" + 
-			"........V......\n" , spaceinvaders.recupererEspaceJeuDansChaineASCII());
-		}
+	   }
 
 	   
 	   @Test
@@ -90,24 +70,7 @@
 			"......V........\n" , spaceinvaders.recupererEspaceJeuDansChaineASCII());
 		}
 	   
-	   @Test
-		public void test_VaisseauImmobile_DeplacerVaisseauVersLaDroite() {
-			
-			spaceinvaders.positionnerUnNouveauVaisseau(1,1,14,9);
-			spaceinvaders.deplacerVaisseauVersLaDroite();
-			
-			assertEquals("" + 
-			"...............\n" + 
-			"...............\n" +
-			"...............\n" + 
-			"...............\n" + 
-			"...............\n" + 
-			"...............\n" + 
-			"...............\n" + 
-			"...............\n" + 
-			"...............\n" + 
-			"..............V\n" , spaceinvaders.recupererEspaceJeuDansChaineASCII());
-		}
+	   
 	   
 	   @Test
 		public void test_VaisseauImmobile_DeplacerVaisseauVersLaGauche() {
@@ -200,6 +163,42 @@
 			} catch (final DebordementEspaceJeuException e) {
 			}
 				
+		}
+	   
+	   @Test
+	    public void test_VaisseauAvance_DeplacerVaisseauVersLaDroite() {
+			
+			spaceinvaders.positionnerUnNouveauVaisseau(3,2,7,9);
+			spaceinvaders.deplacerVaisseauVersLaDroite();
+			assertEquals("" + 
+			"...............\n" + 
+			"...............\n" +
+			"...............\n" + 
+			"...............\n" + 
+			"...............\n" + 
+			"...............\n" + 
+			"...............\n" + 
+			"...............\n" + 
+			"........VVV....\n" + 
+			"........VVV....\n" , spaceinvaders.recupererEspaceJeuDansChaineASCII());
+		}
+	   
+	   @Test
+		public void test_VaisseauImmobile_DeplacerVaisseauVersLaDroite() {
+			
+			spaceinvaders.positionnerUnNouveauVaisseau(3,2,12,9);
+			spaceinvaders.deplacerVaisseauVersLaDroite();
+			assertEquals("" + 
+			"...............\n" + 
+			"...............\n" +
+			"...............\n" + 
+			"...............\n" + 
+			"...............\n" + 
+			"...............\n" + 
+			"...............\n" + 
+			"...............\n" + 
+			"............VVV\n" + 
+			"............VVV\n" , spaceinvaders.recupererEspaceJeuDansChaineASCII());
 		}
 
 }
