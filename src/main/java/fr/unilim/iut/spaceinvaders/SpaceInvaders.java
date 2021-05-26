78eab8cd5847878e6d729355bed5105dad169320
Theo Delias <t.delias@gmail.com>
2021-05-26 23:39:30 +0200

    Fonctionnalité 3 fini

--- src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
@@ -104,10 +104,10 @@ public class SpaceInvaders implements Jeu{
 	} 
 
 	public void initialiserJeu() {
+		
 		 Position positionVaisseau = new Position(this.longueur/2,this.hauteur-1);
 		 Dimension dimensionVaisseau = new Dimension(Constante.VAISSEAU_LONGUEUR, Constante.VAISSEAU_HAUTEUR);
-		 positionnerUnNouveauVaisseau(dimensionVaisseau, positionVaisseau,4);
-		
+		 positionnerUnNouveauVaisseau(dimensionVaisseau, positionVaisseau, Constante.VAISSEAU_VITESSE);
 	}
 	
 	
